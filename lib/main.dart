c3260294c39d6591cdf2038db528fb3d618c3557
PopeJ442 <PopeJ442@users.noreply.github.com>
2023-03-14 18:27:56 +0000

    change

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  static String id = "Hello";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
